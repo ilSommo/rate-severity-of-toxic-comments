0d3ebd469472df410eb754f043b065afc0816f84
Alessandro Stockman <alessandrostockman@gmail.com>
2022-01-17 17:35:27 +0100

    add dummy model for debugging purposes and minor fix

--- rate_severity_of_toxic_comments/model.py
+++ rate_severity_of_toxic_comments/model.py
@@ -1,12 +1,14 @@
+import torch
 from torch import nn
 from transformers import AutoModel
 
-class JigsawModel(nn.Module):
+class PretrainedModel(nn.Module):
     def __init__(self, model_name, num_classes):
-        super(JigsawModel, self).__init__()
+        super().__init__()
         self.model = AutoModel.from_pretrained(model_name)
         self.drop = nn.Dropout(p=0.2)
-        self.fc = nn.Linear(768, num_classes)
+        #TODO: Is pooler.dense.out_features guaranteed to exist?
+        self.fc = nn.Linear(self.model.pooler.dense.out_features, num_classes) 
         
     def forward(self, ids, mask):        
         out = self.model(input_ids=ids,attention_mask=mask,
@@ -14,3 +16,11 @@ class JigsawModel(nn.Module):
         out = self.drop(out[1])
         outputs = self.fc(out)
         return outputs
+
+class DummyModel(nn.Module):
+    def __init__(self, model_name, num_classes):
+        super().__init__()
+        self.fc = nn.Linear(1, num_classes)
+        
+    def forward(self, ids, mask):
+        return self.fc(ids.type(torch.float).mean(dim=0).unsqueeze(1))
