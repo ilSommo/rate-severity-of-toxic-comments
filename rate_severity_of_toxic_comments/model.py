ae51ab31b6f5c81ae93e84b3bffaea3c2eef0b8f
Alessandro Stockman <alessandrostockman@gmail.com>
2022-01-17 17:52:00 +0100

    add create_model function

--- rate_severity_of_toxic_comments/model.py
+++ rate_severity_of_toxic_comments/model.py
@@ -1,14 +1,15 @@
 import torch
 from torch import nn
-from transformers import AutoModel
+
+OUTPUT_CLASSES = 1
 
 class PretrainedModel(nn.Module):
-    def __init__(self, model_name, num_classes):
+    def __init__(self, model_name):
         super().__init__()
         self.model = AutoModel.from_pretrained(model_name)
         self.drop = nn.Dropout(p=0.2)
         #TODO: Is pooler.dense.out_features guaranteed to exist?
-        self.fc = nn.Linear(self.model.pooler.dense.out_features, num_classes) 
+        self.fc = nn.Linear(self.model.pooler.dense.out_features, OUTPUT_CLASSES) 
         
     def forward(self, ids, mask):        
         out = self.model(input_ids=ids,attention_mask=mask,
@@ -18,9 +19,15 @@ class PretrainedModel(nn.Module):
         return outputs
 
 class DummyModel(nn.Module):
-    def __init__(self, model_name, num_classes):
+    def __init__(self):
         super().__init__()
-        self.fc = nn.Linear(1, num_classes)
+        self.fc = nn.Linear(1, OUTPUT_CLASSES)
         
     def forward(self, ids, mask):
         return self.fc(ids.type(torch.float).mean(dim=0).unsqueeze(1))
+
+def create_model(config):
+    if config["run_mode"] == "test":
+        return DummyModel()
+    elif config["run_mode"] == "pretrained":
+        return PretrainedModel(config["model_name"])
