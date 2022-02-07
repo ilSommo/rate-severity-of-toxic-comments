15d0a32b67ff971db1322ef1c0aa48a39e1aaaa2
Lorenzo Menghini <lollomenghini@gmail.com>
2022-02-07 22:39:02 +0100

    Added documentation

--- rate_severity_of_toxic_comments/tokenizer.py
+++ rate_severity_of_toxic_comments/tokenizer.py
@@ -9,6 +9,7 @@ from rate_severity_of_toxic_comments.vocabulary import load_vocabulary, build_vo
 
 
 def create_recurrent_model_tokenizer(config, df, verbose=False):
+
     vocab_file_path = config["recurrent"]["vocab_file"]
     dataframe_cols = config["training"]["dataset"]["cols"]
     embedding_dim = config["recurrent"]["embedding_dimension"]
@@ -24,10 +25,10 @@ def create_recurrent_model_tokenizer(config, df, verbose=False):
     if verbose:
         oov = check_OOV_terms(embedding_model, vocab)
         # count_OOV_frequency(df, dataframe_cols, oov)
-    
+
     embedding_matrix = build_embedding_matrix(
         embedding_model, embedding_dim, vocab)
-    
+
     return tokenizer, embedding_matrix
 
 
