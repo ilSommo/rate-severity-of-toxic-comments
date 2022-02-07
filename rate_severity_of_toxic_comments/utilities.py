4216872324567f1988b5b4ab25c26f46291b145f
Alessandro Stockman <alessandrostockman@gmail.com>
2022-02-07 20:18:24 +0100

    add parse config utility function

--- rate_severity_of_toxic_comments/utilities.py
+++ rate_severity_of_toxic_comments/utilities.py
@@ -2,6 +2,8 @@ __version__ = '0.1.0'
 __author__ = 'Lorenzo Menghini, Martino Pulici, Alessandro Stockman, Luca Zucchini'
 
 import random
+import json
+import os
 
 import numpy as np
 import torch
@@ -116,3 +118,14 @@ def process_config(df, config):
         fix_random_seed(config["options"]["seed"])
 
     return support_bag
+
+def parse_config(default_filepath, local_filepath=None):
+    default = open(default_filepath)
+    CONFIG = json.load(default)
+
+    if os.path.exists(local_filepath):
+        with open(local_filepath) as local:
+            CONFIG.update(json.load(local))
+
+    validate_config(CONFIG)
+    return CONFIG
