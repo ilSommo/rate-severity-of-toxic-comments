34679b6ba9bc20d507612af616651a87046f463d
Alessandro Stockman <alessandrostockman@gmail.com>
2022-02-07 20:25:33 +0100

    add arguments to download_resources

--- download_resources.py
+++ download_resources.py
@@ -28,6 +28,15 @@ def download(file_path, download_url):
         print("File already on file system")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datasets', action="store_true")
+    parser.add_argument('--vocabs', action="store_true")
+    parser.add_argument('--models', action="store_true")
+    args = parser.parse_args()
+
+    if not (args["datasets"] or args["vocabs"] or args["models"]):
+        raise argparse.ArgumentError("Choose at least one resource category to download")
+
     default = open(DEFAULT_CONFIG_FILE_PATH)
     CONFIG = json.load(default)
 
@@ -41,20 +50,24 @@ if __name__ == "__main__":
     models_file = open(BEST_MODELS_FILE_PATH)
     models = json.load(models_file)
 
-    print("Downloading training set file")
-    download(CONFIG["training"]["dataset"]["path"], CONFIG["training"]["dataset"]["download"])
-    print("Downloading test set file")
-    download(CONFIG["evaluation"]["dataset"]["path"], CONFIG["evaluation"]["dataset"]["download"])
+    if args["datasets"]:
+        print("Downloading training set file")
+        download(CONFIG["training"]["dataset"]["path"], CONFIG["training"]["dataset"]["download"])
+        print("Downloading test set file")
+        download(CONFIG["evaluation"]["dataset"]["path"], CONFIG["evaluation"]["dataset"]["download"])
 
-    print("Downloading vocab file")
-    preprocess_vocab = get_preprocess_filenames(CONFIG["recurrent"]["preprocessing"], CONFIG["recurrent"]["vocab_file"])
-    download(preprocess_vocab, vocabs[preprocess_vocab])
+    if args["vocabs"]:
+        print("Downloading vocab file")
+        preprocess_vocab = get_preprocess_filenames(CONFIG["recurrent"]["preprocessing"], CONFIG["recurrent"]["vocab_file"])
+        download(preprocess_vocab, vocabs[preprocess_vocab])
 
-    print("Downloading final models")
-    for model in models:
-        print("Downloading", model["description"])
-        download(model["path"], model["download"])
-    print("Finished")
+    if args["models"]:
+        print("Downloading final models")
+        for model in models:
+            print("Downloading", model["description"])
+            download(model["path"], model["download"])
+    
+    print("Downloading resources finished")
 
 
 
